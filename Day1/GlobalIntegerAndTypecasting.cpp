d8465dfc025ee68d5e93254d31de80973cbc2010
bhavya pathak14 <Bhavyapathak57@gmail.com>
2026-05-05 17:52:40 +0530

    Normal Commit

---
+++ Day1/GlobalIntegerAndTypecasting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int a = 200;
+int main(){
+int n;
+double ans ;
+cout<<"Enter a number :";
+cin>>n;
+ans = (double)a/n;
+cout<< ans << endl;
+return 0;
+}
